2d41fdd7fc19d598ed0928c02d1259e7acdd452e
Claire Roberts <clairefinnie@gmail.com>
2023-02-03 15:15:55 -0500

    basic security fixes

--- public/hello.js
+++
@@ -1,1 +0,0 @@
-alert('we out here')
--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,14 +1,20 @@
 mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
-	container: 'map', // container ID
+	container: 'show-map', // container ID
 	style: 'mapbox://styles/mapbox/light-v10', // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 8, // starting zoom
 })
 
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-left')
+
 new mapboxgl.Marker()
 	.setLngLat(campground.geometry.coordinates)
 	.setPopup(
-		new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3>`)
+		new mapboxgl.Popup({ offset: 25 }).setHTML(
+			`<h3>${campground.title}</h3><p>${campground.location}}</p>`
+		)
 	)
 	.addTo(map)
+
+//different option to set HTML would be via ajax request and so avoid having the campground <%-JsonStringify%> vulnerability...keeping simple and practising sanitzing input here but there is an alternative
